f7f72271df3ca0cfe241dc2e473e92eb943b316d
Alphamoris <alphamoris45@gamil.com>
2024-11-13 01:34:19 +0530

    Partially completed Landing Page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,5 @@
 import { HoverBorderGradientDemo } from "@/components/LoginButton";
 import { Landing } from "@/components/Landing";
-import Dashboard from "@/components/Sidebar";
 import { Signup } from "@/components/Signup";
 import Image from "next/image";
 
